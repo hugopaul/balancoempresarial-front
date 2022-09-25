b66cff2ddd40baa091ce0338bde54974a3f36a68
Hugo Paul <hugo.paul@hotmail.com>
2022-09-25 17:56:09 -0300

    adicionando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InfoEmpresaComponent } from './components/info-empresa/info-empresa.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component: HomeComponent },
+  { path: 'info-empresa' , component: InfoEmpresaComponent },
+  { path: 'usuario' , component: UsuarioComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { GeralComponent } from './components/geral/geral.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { InfoEmpresaComponent } from './components/info-empresa/info-empresa.component';
+import { TemplateComponent } from './components/template/template.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
 
 @NgModule({
@@ -18,7 +21,10 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
     HomeComponent,
     MenuComponent,
     HeaderComponent,
-    GeralComponent
+    GeralComponent,
+    InfoEmpresaComponent,
+    TemplateComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
